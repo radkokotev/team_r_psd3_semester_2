4551744178ebe2233f86ff5411a818d3df583ec0
misho92 <matachev92@yahoo.co.uk>
2014-02-12 13:52:43 +0000

    Update AdministratorRole.java

--- DCS_Timetable/src/roles/AdministratorRole.java
+++ DCS_Timetable/src/roles/AdministratorRole.java
@@ -4,3 +4,6 @@ public interface AdministratorRole {
 	public void assignRoomToSession(String room, String SessionTitle) 
 			throws PermissionsDeniedException;
 }
+// update, necessary for the new user story we were just introduced
+	public void createTimeSlotForSession(Date startTime, Date endTime, String SessionTitle)
+			throws PermissionsDeniedException;
