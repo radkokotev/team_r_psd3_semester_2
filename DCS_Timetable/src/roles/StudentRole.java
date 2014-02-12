9d922fd41dcab20db4fd5249e3d3ebf47bebdcb5
misho92 <matachev92@yahoo.co.uk>
2014-02-12 15:24:59 +0000

    Update StudentRole.java
    additional comments

--- DCS_Timetable/src/roles/StudentRole.java
+++ DCS_Timetable/src/roles/StudentRole.java
@@ -1,5 +1,13 @@
 package roles;
 
+/*
+Team R
+Whole team worked on project (Greblykas,Kotev,Tachev,Turner,Vascila)
+Version 1.0
+this interface it to represent the student object in order to do his/her tasks which are currently boking slot for a course,
+get all compulsory sessions for course and get sessions for which he/she is enrolled into.
+*/
+
 public interface StudentRole {
 	public void bookSlotForCourse(String id, String sessionTitle, String courseTitle)
 			throws PermissionsDeniedException;
