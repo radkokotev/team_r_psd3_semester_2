5d28791005b5dec9b71a48d04fc116a4be6b304d
misho92 <matachev92@yahoo.co.uk>
2014-02-12 15:26:39 +0000

    Update TutorRole.java
    javadoc

--- DCS_Timetable/src/roles/TutorRole.java
+++ DCS_Timetable/src/roles/TutorRole.java
@@ -1,5 +1,13 @@
 package roles;
 
+/*
+Team R
+Whole team worked on project (Greblykas,Kotev,Tachev,Turner,Vascila)
+Version 1.0
+this interface it to represent the tutor who is able to do his/her tasks which is currently only able to get information 
+for a session.
+*/
+
 public interface TutorRole {
 	public String getInformationForSession(String sessionTitle) 
 			throws PermissionsDeniedException;
